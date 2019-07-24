a532c92d2d084788a3c884d3e033a50ee3324635
yi-hou <hou.yi1@husky.neu.edu>
2019-07-24 16:32:19 -0400

    login register works

--- app/src/main/java/com/example/foodo/LoginActivity.java
+++ app/src/main/java/com/example/foodo/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.foodo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -8,32 +9,57 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-    Button loginButton;
-    Button signupButton;
+    private Button loginButton, signupButton;
+    private EditText email, password;
+    private String emailData, passwordData;
+    private FirebaseAuth authenticationRef;
+
+    private Intent logIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-
+        email = findViewById(R.id.email_input);
+        password = findViewById(R.id.password_input);
         loginButton = findViewById(R.id.login_button);
         signupButton = findViewById(R.id.signup_button);
+        authenticationRef = FirebaseAuth.getInstance();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                  Intent explicitIntent = new Intent(LoginActivity.this, MainActivity.class);
-                  startActivity(explicitIntent);
+                  emailData = email.getText().toString();
+                  passwordData = password.getText().toString();
+                  logIn = new Intent(LoginActivity.this, MainActivity.class);
+                  authenticationRef.signInWithEmailAndPassword(emailData, passwordData).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                      @Override
+                      public void onComplete(@NonNull Task<AuthResult> task) {
+                          if(task.isSuccessful()){
+                              startActivity(logIn);
+                          } else {
+                              Toast.makeText(getApplicationContext(), "Login Failed", Toast.LENGTH_SHORT).show();
+                          }
+                      }
+                  });
             }
         });
 
         signupButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-                Intent explicit = new Intent(LoginActivity.this, SignUp.class);
-                startActivity(explicit);
+                Intent goToSignUp = new Intent(LoginActivity.this, SignUp.class);
+                startActivity(goToSignUp);
             }
         });
 
--- app/src/main/java/com/example/foodo/SignUp.java
+++ app/src/main/java/com/example/foodo/SignUp.java
@@ -1,26 +1,54 @@
 package com.example.foodo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
-    Button signup;
-
+    private Button signup;
+    private EditText email, password;
+    private String emailData, passwordData;
+    private FirebaseAuth authenticationRef;
+    private Intent goToSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-
+        email = findViewById(R.id.sign_up_email);
+        password = findViewById(R.id.sign_up_password);
         signup = findViewById(R.id.sign_up);
+
+        authenticationRef = FirebaseAuth.getInstance();
+
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent explicitIntent = new Intent(SignUp.this, LoginActivity.class);
-                startActivity(explicitIntent);
+                emailData = email.getText().toString();
+                passwordData = password.getText().toString();
+                goToSignIn = new Intent(SignUp.this, LoginActivity.class);
+
+                authenticationRef.createUserWithEmailAndPassword(emailData, passwordData).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+                            startActivity(goToSignIn);
+                        }else{
+                            Toast.makeText(getApplicationContext(), "Fail", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
     }
