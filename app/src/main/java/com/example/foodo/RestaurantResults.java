d38442c0166a1d9631733d3ea1bb6b2160f0a2a0
yi-hou <hou.yi1@husky.neu.edu>
2019-07-30 15:28:16 -0400

    yelp api

--- app/src/main/java/com/example/foodo/RestaurantResults.java
+++ app/src/main/java/com/example/foodo/RestaurantResults.java
@@ -6,69 +6,78 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RestaurantResults.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RestaurantResults#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class RestaurantResults extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     ListView restaurantResults;
+
 
     public RestaurantResults() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RestaurantResults.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RestaurantResults newInstance(String param1, String param2) {
-        RestaurantResults fragment = new RestaurantResults();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_restaurant_results, container, false);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+
+        String url = "https://api.yelp.com/v3/businesses/search";
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("resquestFAIL", error.toString());
+                    }
+                }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Authorization", "Bearer"+" "+"xdi6xgTOkvpvYn53XZFXjuAdQ8C_0Lg1bR4vFEA-DItoPp5JV_ROe4BcFOUWwxTwtfX3Pl0NMyjwbVRai1YhGRz3PHg8w22FyVcNjD9GnSlIv11wHoU6MYmcLmM_XXYx");
+                return headers;
+            }
+        };
+
+        requestQueue.add(objectRequest);
+
         restaurantResults = v.findViewById(R.id.restaurant_results);
         restaurantResults.setOnItemClickListener(new ListView.OnItemClickListener() {
             @Override
@@ -83,32 +92,6 @@ public class RestaurantResults extends Fragment {
         return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
 
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
 
 }
